b8f211f5d317a817fbcafb663ac8b4af8e6ebe89
nooha-ayesha <noohaayesha23@gmail.com>
2026-07-25 18:38:54 +0530

    Add Lesson 04: Arithmetic

---
+++ src/lesson04_arithmaticoperations/Main.java
@@ -0,0 +1,68 @@
+package lesson04_arithmaticoperations;
+
+//import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+
+        //int x=0;
+        //int y=0;
+        //int z=0;
+
+        //Scanner scanner = new Scanner(System.in);
+
+        /*
+          arithmatic operations
+          sample code demonstrating about
+          augmented assignment, increment and decrement operations
+          or order of operations.
+         */
+
+        //arithmatic operations
+
+
+        //System.out.println("Enter the value of x: ");
+        //x = scanner.nextInt();
+
+        /*
+        System.out.println("Enter the value of y: ");
+        y = scanner.nextInt();
+
+
+        //z=x+y;
+        //z=x-y;
+        //z=x*y;
+        //z=x/y;
+        //z=x%y;
+
+        System.out.println("The value of z is: " + z);
+
+
+
+        //AUGMENTED ASSIGNMENT OPERATOR
+
+        //x+=y;
+        //x-=y;
+        //x*=y;
+        //x/=y;
+        //x%=y;
+
+        System.out.println("The value of x is:" + x);
+
+
+
+        //INCREMENT & DECREMENT OPERATION
+
+        //x++;
+        //x--;
+        System.out.println("The value of x is:" + x);
+
+       */
+
+        //ORDER OF OPERATIONS [P-E-M-D-A-S]
+
+        double result = 5 + 4 * 6 * (2/2) / 2;
+        System.out.println(result);
+        //scanner.close();
+    }
+}
