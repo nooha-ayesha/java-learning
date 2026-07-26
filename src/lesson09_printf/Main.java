5f1c1767b385373c1c2e8f1f077e2a94b67b1c1f
Ayesha Nooha <noohaayesha23@gmail.com>
2026-07-26 21:33:33 +0530

    Add lesson09: printf

---
+++ src/lesson09_printf/Main.java
@@ -0,0 +1,83 @@
+package lesson09_printf;
+
+public class Main{
+    public static void main(String[] args){
+
+        //printf()
+
+
+        //%[flags][.precision][width][specifier-character]
+
+        //specifier-character
+
+
+        /*
+        String name = "Paddington";
+        char firstLetter = 'P';
+        int age = 26;
+        double weight = 24.6;
+        Boolean isToy = true;
+
+
+        System.out.printf("My name is %s.\n", name);
+        System.out.printf("The first letter of my name starts with %c.\n", firstLetter);
+        System.out.printf("I am %d years old, \n", age);
+        System.out.printf("and I think I weigh around %f\n", weight);
+        System.out.printf("'Am I a toy?': %b", isToy);
+
+        */
+
+
+
+        //flags
+
+        //double price1 = 3400.78;
+        //double price2 = -99000.9;
+
+        /*
+        System.out.printf("The price for item 1 is: %+f", price1);
+        System.out.printf("\nThe price for item 2 is: %+f", price2);
+        System.out.printf("\nThe price for item 1 is: % f", price1);
+        System.out.printf("\nThe price for item 2 is: % f", price2);
+        System.out.printf("\nThe price for item 1 is: %(f", price1);
+        System.out.printf("\nThe price for item 2 is: %(f", price2);
+        System.out.printf("\nThe price for item 1 is: % ,f", price1);
+        System.out.printf("\nThe price for item 2 is: % ,f", price2);
+         */
+
+
+        //.precision
+
+        /*
+        System.out.printf("The price for item 1 is: %.2f", price1);
+        System.out.printf("\nThe price for item 2 is: %.2f", price2);
+        */
+
+        //width
+
+        /*
+        int no1=1;
+        int no2=10;
+        int no3=234;
+        int no4=1027;
+
+        System.out.printf("%-4d", no1);
+        System.out.printf("\n%-4d", no2);
+        System.out.printf("\n%-4d", no3);
+        System.out.printf("\n%-4d", no4);
+
+        System.out.printf("\n%4d", no1);
+        System.out.printf("\n%4d", no2);
+        System.out.printf("\n%4d", no3);
+        System.out.printf("\n%4d", no4);
+
+        System.out.printf("\n%04d", no1);
+        System.out.printf("\n%04d", no2);
+        System.out.printf("\n%04d", no3);
+        System.out.printf("\n%04d", no4);
+        */
+
+
+    }
+
+}
