192498154e2711c148df81863b42c434a7fa41d1
Ayesha Nooha <noohaayesha23@gmail.com>
2026-07-27 23:55:26 +0530

    Add Lesson12: String Methods

---
+++ src/lesson12_string_methods/Main.java
@@ -0,0 +1,92 @@
+package lesson12_string_methods;
+
+
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+
+        //STRING METHODS
+
+        //Scanner scanner = new Scanner(System.in);
+
+        //String name;
+
+        /*
+        System.out.println("Enter your FIRST and LAST name: ");
+        name = scanner.nextLine();
+
+        char firstLetter = name.charAt(0);
+        System.out.print("The first letter of your name is: " + firstLetter);
+
+        int length = name.length();
+        char middle =name.charAt(length / 2);
+
+        System.out.print("\nThe middle letter is: " + middle);
+
+        char lastLetter = name.charAt(length - 1);
+        System.out.print("\nThe last letter of your name is: " + lastLetter);
+
+        name = name.toLowerCase();
+        System.out.print("\nYour name in 'lowercase': " + name);
+
+        name = name.toUpperCase();
+        System.out.print("\nYour name in 'UPPERCASE': " + name);
+
+        name = name.trim();
+        System.out.print("\nYour name without any EXTRA spaces:" + name);
+
+        scanner.close();
+       */
+
+
+        /*
+        String name = "AYESHA NOOHA";
+
+        int index = name.indexOf("O");
+
+        System.out.print("Index of O: " + index);
+
+        int lastIndex = name.lastIndexOf("O");
+        System.out.print("\nIndex of second O: " + lastIndex);
+
+        name = name.replace('E', 'I');
+        System.out.print("\nreplaced:" + name);
+
+        if(name.isEmpty()){
+            System.out.print("You DIDN'T enter your name");
+        }
+        else{
+            System.out.println("\nHello, " + name);
+        }
+
+        if(name.contains("E")){
+            System.out.println("\nYour name contains letter 'E'");
+        }
+        else{
+            System.out.println("Your name DOESN'T contain a letter 'E'");
+        }
+
+        if(name.equals("AYESHA NOOHA")){
+            System.out.println("HELLO, " + name);
+        }
+        else{
+            System.out.println("INCORRECT NAME!");
+        }
+
+        if(name.equalsIgnoreCase("AYishA nOoHa")){
+            System.out.println("Your name is: " + name);
+        }
+        else{
+            System.out.println("INCORRECT!");
+        }
+
+        if(name.equalsIgnoreCase("AYishA ")){
+            System.out.println("Your name is: " + name);
+        }
+        else{
+            System.out.println("INCORRECT!");
+        }
+
+         */
+    }
+}
