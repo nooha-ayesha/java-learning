8e057c56e04b77922a5f08899afc69c0a7e263f1
Ayesha Nooha <noohaayesha23@gmail.com>
2026-07-28 00:17:33 +0530

    Add Lesson13: Substring

---
+++ src/lesson13_substring/Main.java
@@ -0,0 +1,41 @@
+package lesson13_substring;
+
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        String email;
+        String domain;
+        String username;
+
+        System.out.println("Enter your email: ");
+        email = scanner.nextLine();
+
+        /*
+        username = email.substring(0, email.indexOf("@"));
+        System.out.print("username: " + username);
+
+        domain = email.substring(email.indexOf("@") + 1);
+        System.out.println("\nDomain: " + domain);
+
+         */
+
+        if(email.contains("@")){
+            username = email.substring(0, email.indexOf("@"));
+            System.out.print("username: " + username);
+
+            domain = email.substring(email.indexOf("@") + 1);
+            System.out.println("\nDomain: " + domain);
+        }
+        else{
+            System.out.println("Email must contain @!!");
+        }
+
+
+
+        scanner.close();
+
+    }
+}
